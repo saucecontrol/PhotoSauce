10b06425a008cfee6f258ef04f4e2b874a769955
Rick Brewster <rickbrew@users.noreply.github.com>
2023-11-02 11:05:30 -0700

    Add TransformFactory.CreateScalerTransform()
    This enables me to have more customized rendering pipelines at full BGRA128Float precision.

--- src/MagicScaler/Experimental/TransformFactory.cs
+++ src/MagicScaler/Experimental/TransformFactory.cs
@@ -1,7 +1,6 @@
 // Copyright © Clinton Ingram and Contributors.  Licensed under the MIT License.
 
 using System;
-
 using PhotoSauce.MagicScaler.Transforms;
 
 namespace PhotoSauce.MagicScaler.Experimental;
@@ -20,5 +19,27 @@ public static class TransformFactory
 		ColorProfile? sourceProf = sourceProfile.IsEmpty ? null : ColorProfile.Parse(sourceProfile);
 		ColorProfile? destProf = destProfile.IsEmpty ? null : ColorProfile.Parse(destProfile);
 		return new ConversionTransform(source.AsPixelSource(), PixelFormat.FromGuid(destFormat), sourceProf, destProf);
+	}
+
+	/// <summary>Creates a transform that resizes an <see cref="IPixelSource"/> to the given size, optionally using a specific interpolator.</summary>
+	/// <param name="source">The <see cref="IPixelSource"/> to retrieve pixels from.</param>
+	/// <param name="newWidth">The width of the resulting <see cref="IPixelSource"/>.</param>
+	/// <param name="newHeight">The height of the resulting <see cref="IPixelSource"/>.</param>
+	/// <param name="interpolationSettings">The interpolation settings to use. If <c>null</c>, the default high-quality scaler will be used.</param>
+	/// <returns>An <see cref="IPixelSource"/> that provides the resulting pixel data.</returns>
+	public static IPixelSource CreateScalerTransform(IPixelSource source, int newWidth, int newHeight, InterpolationSettings? interpolationSettings)
+	{
+		if (interpolationSettings is null)
+		{
+			double scaleRatio = Math.Min(
+				source.Width > 0 ? (double)newWidth / source.Width : 0, 
+				source.Height > 0 ? (double)newHeight / source.Height : 0);
+			InterpolationSettings defaultInterpolationSettings = SettingsUtil.GetDefaultInterpolation(scaleRatio);
+			return CreateScalerTransform(source, newWidth, newHeight, defaultInterpolationSettings);
+		}
+		else
+		{
+			return ConvolutionTransform.CreateResample(source.AsPixelSource(), newWidth, newHeight, interpolationSettings.Value, interpolationSettings.Value);
+		}
 	}
 }
