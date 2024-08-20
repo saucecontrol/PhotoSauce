f859961ec3f0086038a2d6bc13739d61ea3c6c22
Rick Brewster <rickbrew@users.noreply.github.com>
2024-08-19 18:56:09 -0700

    Implement IColorProfileHandle for TransformFactory.CreateConversionTransform() (#163)

---
+++ src/MagicScaler/Experimental/ColorProfileHandle.cs
@@ -0,0 +1,25 @@
+// Copyright © Clinton Ingram and Contributors.  Licensed under the MIT License.
+
+using System;
+
+namespace PhotoSauce.MagicScaler.Experimental;
+
+/// <summary>A handle to a color profile for use with other experimental APIs.</summary>
+public interface IColorProfileHandle
+{
+	internal ColorProfile ColorProfile { get; }
+}
+
+/// <summary>Provides factory methods for <see cref="IColorProfileHandle"/>.</summary>
+public static class ColorProfileHandle
+{
+	/// <summary>Given an ICC profile, creates an <see cref="IColorProfileHandle"/>.</summary>
+	/// <param name="profile">The ICC color profile.</param>
+	/// <returns>An <see cref="IColorProfileHandle"/> for use with other experimental APIs.</returns>
+	public static IColorProfileHandle Create(ReadOnlySpan<byte> profile) => new Impl(ColorProfile.Cache.GetOrAdd(profile));
+
+	private sealed class Impl(ColorProfile profile) : IColorProfileHandle
+	{
+		public ColorProfile ColorProfile { get; } = profile;
+	}
+}
